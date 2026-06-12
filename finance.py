a18f356075c38e44ca2b5b1afc56e4f6fb52df4e
priyalagrawal378 <priyalagrawal@gmail.com>
2026-06-13 03:47:48 +0530

    improve finance calculator

--- finance.py
+++ finance.py
@@ -1,4 +1,9 @@
 name = input("what is your name? ")
 salary=float(input("enter your monthly salary:"))
 savings=salary*0.20
-print(f"hey {name} save rs {savings} this month.")
+expenses= salary-savings
+print ( f"\n hello {name}")
+print ( f"\n Monthly salary: Rs {salary}")
+print ( f"\n Savings (20%) : Rs {savings}")
+print ( f"\n Money available for spending: Rs {expenses}")
+
